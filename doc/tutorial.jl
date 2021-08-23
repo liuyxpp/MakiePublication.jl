1ce64c9355207ae13268c98bc3775d4ffe44b9be
Yi-Xin Liu <lyx@fudan.edu.cn>
2021-08-23 17:48:32 +0800

    Remove [compat] section temporarily

--- doc/tutorial.jl
+++ doc/tutorial.jl
@@ -7,15 +7,10 @@ using InteractiveUtils
 # ╔═╡ 0da325a8-d6f6-4963-8562-9b7847878c07
 begin
 	using Pkg
+	Pkg.add("CairoMakie")
+	Pkg.add("LaTeXStrings")
 	Pkg.add(url="/Users/lyx/SynologyDrive/Develop/MakiePublication")
-end
-
-# ╔═╡ 9e3e5804-03f3-11ec-0e26-3916bdce065f
-begin
-	using CairoMakie
-	using LaTeXStrings
 end
 
 # ╔═╡ Cell order:
 # ╠═0da325a8-d6f6-4963-8562-9b7847878c07
-# ╠═9e3e5804-03f3-11ec-0e26-3916bdce065f
